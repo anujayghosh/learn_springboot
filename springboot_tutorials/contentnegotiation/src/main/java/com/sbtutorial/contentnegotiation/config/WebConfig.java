898ab2176c21d8f94710b4fbb9b150c4f23fb421
Anujay Ghosh <anujay.ghosh@gmail.com>
2021-07-07 16:58:59 +0530

    Content negotiation basic program impl

---
+++ springboot_tutorials/contentnegotiation/src/main/java/com/sbtutorial/contentnegotiation/config/WebConfig.java
@@ -0,0 +1,25 @@
+package com.sbtutorial.contentnegotiation.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
+import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class WebConfig implements WebMvcConfigurer{
+
+	@Override
+	public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
+		
+		
+		//set path extension to true - depreciated to discourage path extension as method of content negotiation
+		//favorPathExtension(true).
+		configurer.favorParameter(true).
+		ignoreAcceptHeader(true).
+		defaultContentType(MediaType.APPLICATION_JSON).
+		mediaType("xml", MediaType.APPLICATION_XML).
+		mediaType("json", MediaType.APPLICATION_JSON);
+	}
+}
---
+++ springboot_tutorials/contentnegotiation/src/test/java/com/sbtutorial/contentnegotiation/ContentnegotiationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sbtutorial.contentnegotiation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContentnegotiationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
