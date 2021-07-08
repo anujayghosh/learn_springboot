b0c5bb1609e8a542a074050071db525c84bc128b
Anujay Ghosh <anujay.ghosh@gmail.com>
2021-07-08 17:29:05 +0530

    Impl POST Method to create new User

--- springboot_tutorials/restfulwebservicesdemo/src/main/java/com/sbtutorial/restfulwebservicesdemo/server/main/UserResource.java
+++ springboot_tutorials/restfulwebservicesdemo/src/main/java/com/sbtutorial/restfulwebservicesdemo/server/main/UserResource.java
@@ -1,10 +1,15 @@
 package com.sbtutorial.restfulwebservicesdemo.server.main;
 
+import java.net.URI;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.sbtutorial.restfulwebservicesdemo.server.main.model.User;
 
@@ -22,6 +27,22 @@ public class UserResource {
 	@GetMapping("/users/{id}")
 	public User retriveUser(@PathVariable int id) {
 		return service.findOne(id);
+	}
+	
+	//method that posts a new user detail   
+//	@PostMapping("/users")  
+//	public String createUser(@RequestBody User user)  
+//	{  
+//		User savedUser=service.save(user);  
+//		return "Success: New user added successfully.";
+//	}  
+	
+	@PostMapping("/users")  
+	public ResponseEntity<Object> createUser(@RequestBody User user)  
+	{  
+	User savedUser=service.save(user);    
+	URI location=ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(savedUser.getId()).toUri();  
+	return ResponseEntity.created(location).build();  
 	}
 
 }
