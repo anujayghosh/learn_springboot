3073060d7928b7af844a7b7dcac8fc15d328c07f
Anujay Ghosh <anujay.ghosh@gmail.com>
2021-07-16 19:27:03 +0530

    Added JAXB Implementation Annotations @XmlRootElement @XmlAttribute @XmlElement

--- springboot_tutorials/contentnegotiation/src/main/java/com/sbtutorial/contentnegotiation/model/Employee.java
+++ springboot_tutorials/contentnegotiation/src/main/java/com/sbtutorial/contentnegotiation/model/Employee.java
@@ -1,5 +1,7 @@
 package com.sbtutorial.contentnegotiation.model;
 
+import javax.xml.bind.annotation.XmlAttribute;
+import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlRootElement;
 
 @XmlRootElement
@@ -13,24 +15,31 @@ public class Employee {
 		super();
 	}
 	
+	@XmlAttribute(name="id")  
 	public String getEmpId() {
 		return empId;
 	}
 	public void setEmpId(String empId) {
 		this.empId = empId;
 	}
+	
+	@XmlElement(name="name")  
 	public String getName() {
 		return name;
 	}
 	public void setName(String name) {
 		this.name = name;
 	}
+	
+	@XmlElement(name="designation")  
 	public String getDesignation() {
 		return designation;
 	}
 	public void setDesignation(String designation) {
 		this.designation = designation;
 	}
+	
+	@XmlElement(name="salary")  
 	public double getSalary() {
 		return salary;
 	}
