373fda69ca9b1eaceb1daae58bbea6577fc9ed69
Anujay Ghosh <anujay.ghosh@gmail.com>
2021-07-19 17:44:27 +0530

    RestTemplate Implementation GET, POST, PUT, DELETE for Employee Details

---
+++ springboot_tutorials/restfulclient/src/test/java/com/sbtutorial/restfulclient/RestfulclientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sbtutorial.restfulclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestfulclientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
